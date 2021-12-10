2cca58b02107756a93590781311687d5bb664802
Chuichui <guchuichui@yeah.net>
2021-12-10 15:45:19 +0800

    master first

--- src/main/java/git/Git.java
+++ src/main/java/git/Git.java
@@ -5,6 +5,7 @@ public class Git {
         System.out.println("hello");
         System.out.println("master");
         System.out.println("hot-fix");
+        System.out.println("啦啦啦啦");
 
     }
 }
